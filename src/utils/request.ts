b886350204d0517fa545eb9337d6346f6a00a617
xuwentao93 <a15157756529@gmail.com>
2022-05-03 15:29:15 +0800

    feat: 更换域名链接

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
   timeout: 10000,
   // baseURL: 'http://www.xuwentao.com',
-  baseURL: 'https://www.xuwentao.com',
+  baseURL: 'https://www.xuwentao.com:8080',
   headers: {
     'Content-type': 'application/json'
   }
