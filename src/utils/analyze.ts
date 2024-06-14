49b918f5eafc4c13d14e8ae62926d5c11815e3c6
xuwentao93 <a15157756529@gmail.com>
2024-06-14 22:37:36 +0800

    feat: 移除面试代码, 修复 eslint

--- src/utils/analyze.ts
+++ src/utils/analyze.ts
@@ -1,4 +1,6 @@
-const score_with_time: { time: number, score: number }[] = []; // 假设 score_with_time 是一个存储首屏加载时间相关信息的数组
+// 全自动首屏时间采集方案, 非常核心的技术点.
+
+const scoureWithTime: { time: number, score: number }[] = []; // 假设 scoureWithTime 是一个存储首屏加载时间相关信息的数组
 const CHECK_INTERVAL = 1000; // 假设 CHECK_INTERVAL 是一个表示检查间隔的时间值
 
 export function CScor(el: Element, tiers: number, parentScore = 0): number {
@@ -24,7 +26,7 @@ export function CScor(el: Element, tiers: number, parentScore = 0): number {
 }
 
 function filterTagNameInTagNames(tagName: string): boolean {
-    return ['SCRIPT', 'STYLE', 'META', 'HEAD'].some((tag) => tag === tagName);
+  return ['SCRIPT', 'STYLE', 'META', 'HEAD'].some(tag => tag === tagName);
 }
 
 function calFinalScore() {
@@ -32,34 +34,34 @@ function calFinalScore() {
   const time = performance.now();
   // 结束判断, 达到一下任意条件结束:
   // 1. 超过 8s. 2. 计算了 4 轮且分数 1s 内没有变化. 3. 计算 9 次分数没有变化.
-  const { length } = score_with_time;
+  const { length } = scoureWithTime;
   let isCheckFMP = time > 8000
-  || (length > 4 && time - (score_with_time[length - 1]?.time || 0) > 2 * CHECK_INTERVAL)
+  || (length > 4 && time - (scoureWithTime[length - 1]?.time || 0) > 2 * CHECK_INTERVAL)
   || (length > 10 && window.performance.timing.loadEventEnd !== 0 &&
-      score_with_time[length - 1]?.score === score_with_time[length - 1]?.score);
+      scoureWithTime[length - 1]?.score === scoureWithTime[length - 2]?.score);
 
   if (isCheckFMP) {
     stop = true;
     // 取 FMP 时间，默认是 30001 大于 30s 会自动被过滤
-    var imgElements = document.querySelectorAll('img');
+    const imgElements = document.querySelectorAll('img');
 
     // 遍历所有的 img 标签元素
-    imgElements.forEach(function(imgElement) {
+    imgElements.forEach(imgElement => {
       // 获取图片的 URL
-      var imgUrl = imgElement.src;
+      const imgUrl = imgElement.src;
       // 创建一个 Image 对象
-      var img = new Image();
+      const img = new Image();
 
       // 记录请求开始时间
-      var startTime = performance.now();
+      const startTime = performance.now();
 
       // 设置图片加载完成的事件处理程序
-      img.onload = function() {
+      img.onload = () => {
         // 记录请求结束时间
-        var endTime = performance.now();
+        const endTime = performance.now();
 
         // 计算请求持续时间
-        var duration = endTime - startTime;
+        const duration = endTime - startTime;
 
         // 输出图片请求信息
         console.log('图片 URL：', imgUrl);
@@ -69,7 +71,7 @@ function calFinalScore() {
       };
 
       // 设置图片加载失败的事件处理程序
-      img.onerror = function() {
+      img.onerror = () => {
         // 输出加载失败信息
         console.log('图片加载失败：', imgUrl);
       };
@@ -77,8 +79,7 @@ function calFinalScore() {
       // 设置图片的 URL
       img.src = imgUrl;
     });
-
-    }
+  }
   return stop;
 }
 
@@ -86,9 +87,9 @@ export function analyzeDOM(dom: Node) {
   const observer = new MutationObserver(mutations => {
     const time = performance.now();
     let baseScore = 0;
-    let fatherNode: Node | ParentNode | null = mutations[0].target;
+    let fatherNode: Node| null = mutations[0].target;
     while (fatherNode) {
-      if (fatherNode.id === 'root') {
+      if ((fatherNode as (Node & { id: string })).id === 'root') {
         break;
       }
       fatherNode = fatherNode.parentNode;
@@ -96,7 +97,7 @@ export function analyzeDOM(dom: Node) {
     }
     console.log(mutations);
     const score = CScor(mutations[0].target as Element, 0, baseScore);
-    score_with_time.push({
+    scoureWithTime.push({
       score,
       time
     });
@@ -106,18 +107,17 @@ export function analyzeDOM(dom: Node) {
       observer.disconnect();
     }
   });
-  
+
   const config = {
     attributes: true, // 监听属性变化
     childList: true, // 监听子节点变化
-  //   characterData: true, // 监听文本内容变化
+    // characterData: true, // 监听文本内容变化
     subtree: true // 监听后代节点变化
   };
-  
-  
+
   observer.observe(dom, config);
 
   window.onload = () => {
     console.log('window.onload', performance.now());
-  }
+  };
 }
